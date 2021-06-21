92350c55a87e11cb93fb173f47ed5e26181c9c75
Cameron <camcreel@gmail.com>
2021-06-20 21:04:49 -0400

    Did deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,31 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+  
+  function onChangeType(e) {
+    setFilters({...filters,type: e.target.value })
+  }
 
+  function onFindPetsClick() {
+    let optionalParamter = filters.type
+    if (optionalParamter === "all") {
+        optionalParamter = ''
+    } else {
+      optionalParamter = '?type=' + optionalParamter
+    }
+    fetch(`http://localhost:3001/pets${optionalParamter}`)
+      .then(resp => resp.json())
+      .then(setPets)
+  }
+
+  function onAdoptPet(id){
+    const shelterPets = pets.map((pet) =>{
+      if (pet.id === id) return {...pet,isAdopted: true}
+      return pet
+    })
+    setPets(shelterPets)
+    
+  }
   return (
     <div className="ui container">
       <header>
@@ -15,10 +39,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
